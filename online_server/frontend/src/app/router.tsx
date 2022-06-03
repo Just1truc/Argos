a47661d1eaf8dea617eed158a9c8d6980913d42b
izimio <joshua.brionne@gmail.com>
2022-06-03 16:55:04 +0200

    [INIT] pages shell

--- online_server/frontend/src/app/router.tsx
+++ online_server/frontend/src/app/router.tsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Login from "../pages/Login";
 import NotFound from "../pages/404";
+import Shell from "../pages/shell";
 
 const Router = (): JSX.Element => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<Login />} />
-
+                <Route path="/shell" element={<Shell />} />
                 {/* 404 Not Found page */}
                 <Route element={<NotFound />} />
                 <Route path='*' element={<NotFound />} />
---
+++ online_server/frontend/src/pages/shell.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+
+
+const Shell = (props:any): JSX.Element => {
+
+    return (
+        <>
+            <h1>Login</h1>
+        </>
+    );
+}
+
+export default Shell;
